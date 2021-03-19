41008634cb442ce273382553068057df6f1af8b5
mittal <mittalkadia2021@gmail.com>
2021-03-19 22:12:24 +0000

    New file add

---
+++ mittal.java
@@ -0,0 +1,12 @@
+public class mittal {
+    public static void main(String[] args) {
+
+        System.out.println("I like software testing");
+        System.out.println("My bithday date is 23");
+        System.out.println("I love india");
+        System.out.println("2+5=7");
+        System.out.println("my homework done");
+
+
+    }
+}
